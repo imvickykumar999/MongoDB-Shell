992ed3169487923a23da725ea425d54061df953c
imvickykumar999 <imvickykumar999@gmail.com>
2023-01-12 17:56:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 # pip install Flask-PyMongo
+# https://stackabuse.com/integrating-mongodb-with-flask-using-flask-pymongo/
 
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
@@ -7,12 +8,20 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
+db = mongo.db
 
 @app.route('/')
 def home_page():
-    online_users = mongo.db.users.find({'online': True})
+    mydict = { "name": "John", "address": "Highway 37" }
+
+    x = db.todos.insert_one(mydict)
+    print(x.inserted_id)
+
+    for x in db.todos.find():
+        print(x)
+
     return render_template('index.html', 
-                            online_users=online_users)
+                            online_users=list(db.todos.find()))
 
 @app.route("/user/<username>")
 def user_profile(username):
