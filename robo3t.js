246d4a4f7f94a6fa1f15b919b1b2811fcf3869bd
imvickykumar999 <imvickykumar999@gmail.com>
2023-01-12 16:42:33 +0530

    pymongodb

---
+++ robo3t.js
@@ -0,0 +1,26 @@
+db.posts.insertMany([  
+  {
+    title: "Post Title 2",
+    body: "Body of post.",
+    category: "Event",
+    likes: 2,
+    tags: ["news", "events"],
+    date: Date()
+  },
+  {
+    title: "Post Title 3",
+    body: "Body of post.",
+    category: "Technology",
+    likes: 3,
+    tags: ["news", "events"],
+    date: Date()
+  },
+  {
+    title: "Post Title 4",
+    body: "Body of post.",
+    category: "Event",
+    likes: 4,
+    tags: ["news", "events"],
+    date: Date()
+  }
+])
